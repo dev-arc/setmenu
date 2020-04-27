1186afa89ff5cc83361563e413b3847c5bfe37d3
Adam Jones <adam.jones@frozenmountain.com>
2020-04-26 22:41:56 -0700

    Add tests for RecipeListItem and RecipeModal
    Split Store into separate files for convenience later.

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-/* eslint-disable no-param-reassign */
 import Vue from 'vue';
 import Vuex from 'vuex';
+import actions from './actions';
+import getters from './getters';
+import mutations from './mutations';
 import defaultRecipes from './defaultRecipes';
-import { uuid, saveStatePlugin } from '../util/util';
+import { saveStatePlugin } from '../util/util';
 
 Vue.use(Vuex);
 
@@ -11,31 +13,8 @@ export default new Vuex.Store({
   state: {
     recipes: JSON.parse(localStorage.getItem('recipes')) || defaultRecipes,
   },
-  getters: {
-    getRecipe(state) {
-      return (id) => {
-        const recipe = state.recipes.filter((r) => r.id === id);
-        return recipe.length > 0 ? recipe[0] : null;
-      };
-    },
-  },
-  mutations: {
-    CREATE_RECIPE: (state, {
-      id, title, subtitle, description,
-    }) => {
-      state.recipes.push({
-        id: id ?? uuid(),
-        title: title ?? '',
-        subtitle: subtitle ?? '',
-        description: description ?? '',
-      });
-    },
-    UPDATE_RECIPE_PROPERTY: (state, { recipe, key, value }) => {
-      recipe[key] = value;
-    },
-  },
-  actions: {
-  },
-  modules: {
-  },
+  getters,
+  mutations,
+  actions,
+  modules: {},
 });
---
+++ tests/unit/testRecipeModal.spec.js
@@ -0,0 +1,51 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import RecipeModal from '@/components/RecipeModal.vue';
+import defaultRecipe from './testRecipeModel';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const validId = defaultRecipe.id;
+const invalidId = `${defaultRecipe}invalid`;
+
+describe('RecipeModal.vue', () => {
+  let getters;
+  let store;
+
+  beforeEach(() => {
+    getters = {
+      getRecipe() {
+        return (id) => (id === validId ? defaultRecipe : null);
+      },
+    };
+    store = new Vuex.Store({
+      getters,
+    });
+  });
+
+  /**
+   * This tests a computed property that's directly passed to FormInput
+   */
+  it('title of correct recipe is found', () => {
+    const wrapper = shallowMount(RecipeModal, {
+      store,
+      localVue,
+      propsData: { id: validId },
+    });
+    const { title } = wrapper.vm.recipe;
+    expect(title).toBe(defaultRecipe.title);
+  });
+
+  /**
+   * This tests a computed property that fails in the getter
+   */
+  it('when no recipe found displays "No Recipe found"', () => {
+    const wrapper = shallowMount(RecipeModal, {
+      store,
+      localVue,
+      propsData: { id: invalidId },
+    });
+    expect(wrapper.text()).toContain('No Recipe found');
+  });
+});
